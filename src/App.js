40d516401218440ca94f7bdbd8ab4750e920d9c0
Farid Amimri <farid.amimri@gmail.com>
2023-01-02 12:30:22 +0100

    Create Navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 /** @format */
 
+import styled from 'styled-components'
+import Navbar from './components/Navbar'
+
 function App() {
-  return <div className='App'>Agency</div>
+  return (
+    <Container className='App'>
+      <Navbar />
+    </Container>
+  )
 }
 
 export default App
+
+const Container = styled.div`
+  height: 100vh;
+`
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,72 @@
 /** @format */
 
-import React from 'react'
+import styled from 'styled-components'
+import Button from '@mui/material/Button'
 
 function Navbar() {
-  return <div>Navbar</div>
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Logo>Majesctic</Logo>
+        </Left>
+        <Center>
+          <Menu>
+            <MenuItem>Nos services</MenuItem>
+            <MenuItem>Nos guides</MenuItem>
+            <MenuItem>Nos offres</MenuItem>
+          </Menu>
+        </Center>
+        <Right>
+          <Button variant='outlined'>Contactez-nous</Button>
+        </Right>
+      </Wrapper>
+    </Container>
+  )
 }
 
 export default Navbar
+
+const Container = styled.div`
+  height: 50px;
+`
+
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+`
+
+const Left = styled.div`
+  flex: 1;
+`
+
+const Logo = styled.h1`
+  font-weight: bold;
+`
+
+const Center = styled.div`
+  flex: 2;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const Menu = styled.ul`
+  display: flex;
+  list-style: none;
+`
+
+const MenuItem = styled.li`
+  margin-right: 30px;
+  font-size: 20px;
+  font-weight: bold;
+  color: grey;
+`
+
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+`
