b496454d845f153acb1478ab7e800edf91c02c43
Farid Amimri <farid.amimri@gmail.com>
2023-01-05 10:23:23 +0100

    Add Footer section

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Feature from './components/Feature'
 import Service from './components/Service'
 import Price from './components/Price'
 import Contact from './components/Contact'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -30,6 +31,7 @@ function App() {
       </Container>
       <Container>
         <Contact />
+        <Footer />
       </Container>
     </>
   )
