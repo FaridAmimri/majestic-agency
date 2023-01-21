af7ea422584555ef6fcd941d9a32526af5e4c22e
Farid Amimri <farid.amimri@gmail.com>
2023-01-21 15:54:47 +0100

    Update content & remove contact info + button

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -1,36 +1,15 @@
 /** @format */
 
 import styled from 'styled-components'
-import Button from '@mui/material/Button'
-import PhoneIcon from '@mui/icons-material/Phone'
-import IntroIcon from '../assets/intro.png'
 import Animation from './Animation'
 
 function Introduction() {
   return (
     <Container>
       <Left>
-        <Title>La solution pour votre entreprise</Title>
-        <Description>
-          Entreprendre l'esprit tranquille passe par la domiciliation de votre
-          entreprise chez Majestic.
-        </Description>
-        <Information>
-          <Button variant='contained' size='medium'>
-            <a href='#contact'> Domicilier mon entreprise</a>
-          </Button>
-          <Contact>
-            <ContactPhone>
-              <PhoneIcon />
-              <PhoneNumber>09 86 12 18 21</PhoneNumber>
-            </ContactPhone>
-            <ContactText>Besoin d'aide ?</ContactText>
-          </Contact>
-        </Information>
+        <Title>La solution qui change la vie</Title>
+        <Description>Vous en avez rêvé, on l'a fait</Description>
       </Left>
-      <Right>
-        <Image src={IntroIcon} />
-      </Right>
       <Animation />
     </Container>
   )
@@ -53,6 +32,7 @@ const Container = styled.div`
 
 const Left = styled.div`
   width: 60%;
+  height: 80%;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -93,72 +73,5 @@ const Description = styled.p`
   }
   @media only screen and (min-width: 481px) and (max-width: 768px) {
     width: 100%;
-  }
-`
-
-const Information = styled.div`
-  margin-top: 50px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-
-  @media only screen and (max-width: 480px) {
-    width: 100%;
-    flex-direction: column;
-  }
-  @media only screen and (min-width: 481px) and (max-width: 768px) {
-    width: 100%;
-    flex-direction: column;
-    align-items: flex-start;
-  }
-`
-
-const Contact = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding-left: 30px;
-
-  @media only screen and (max-width: 480px) {
-    margin-top: 20px;
-  }
-  @media only screen and (min-width: 481px) and (max-width: 768px) {
-    margin-top: 20px;
-    padding: 0;
-  }
-`
-
-const ContactPhone = styled.span`
-  color: #f0667d;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  font-weight: bold;
-`
-
-const PhoneNumber = styled.p`
-  font-size: 20px;
-  padding-left: 5px;
-`
-
-const ContactText = styled.span`
-  color: gray;
-  margin-top: 5px;
-`
-
-const Right = styled.div`
-  width: 40%;
-
-  @media only screen and (max-width: 480px) {
-    display: none;
-  }
-  @media only screen and (min-width: 481px) and (max-width: 768px) {
-    align-self: flex-end;
-  }
-`
-
-const Image = styled.img`
-  width: 115%;
-  @media only screen and (max-width: 480px) {
-    width: 50%;
   }
 `
