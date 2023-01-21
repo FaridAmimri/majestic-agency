664f5313b944a424fe7d766409c5259d7f74d1a8
Farid Amimri <farid.amimri@gmail.com>
2023-01-21 15:50:18 +0100

    Update content + add icons

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -1,31 +1,55 @@
 /** @format */
 
 import styled from 'styled-components'
-import FeatureIcon from '../assets/feature.png'
 import Button from '@mui/material/Button'
 import Animation from './Animation'
+import FeatureIcon from '../assets/feature.png'
+import PuzzledIcon from '../assets/puzzled.png'
+import MessyIcon from '../assets/messy.png'
+import DiscreetIcon from '../assets/discreet.png'
 
 function Feature() {
   return (
     <Container id='feature'>
       <Left>
-        <Image src={FeatureIcon} />
+        <Image src={FeatureIcon} alt='Main avec smartphone' />
       </Left>
       <Right>
         <Title>
-          <b>Gagnez</b> en crédibilité
-          <br />
-          <b>Gagnez</b> en productivité
+          <b>Gagnez</b> du temps grâce à notre domiciliation nouvelle génération
+          et ses avantages exclusifs
         </Title>
-        <SubTitle>
-          Une adresse à Dunkerque à partir de 14€ HT. <br />
-          Une gestion de votre courrier et l'accès rapide à tous documents triés
-          et numérisés sur votre mobile.
-        </SubTitle>
-        <Description>
-          Affranchissez vous des contraintes matérielles pour avoir un maximum
-          de souplesse dans le dimensionnement de votre entreprise.
-        </Description>
+        <Advantage>
+          <Picto src={PuzzledIcon} alt='Femme perplexe' />
+          <Item>
+            <SubTitle>Ne perdez jamais votre courrier !</SubTitle>
+            <Description>
+              Avec Majestic domiciliation vos courriers sont tous réceptionnés,
+              placés dans un espace sécurisé ou réexpédiés là où vous le
+              désirez.
+            </Description>
+          </Item>
+        </Advantage>
+        <Advantage>
+          <Picto src={MessyIcon} alt='Désordre papiers' />
+          <Item>
+            <SubTitle>Finissez-en avec la paperasse</SubTitle>
+            <Description>
+              Grâce à la numérisation et au stockage dropbox, ayez accès à tous
+              vos courrier, sans vous déplacer et sans le papier.
+            </Description>
+          </Item>
+        </Advantage>
+        <Advantage>
+          <Picto src={DiscreetIcon} alt='Avatar discret' />
+          <Item>
+            <SubTitle>Protégez votre vie privé</SubTitle>
+            <Description>
+              Posséder une adresse de domiciliation, c'est aussi une vraie
+              sécurité à apporter à votre foyer.
+            </Description>
+          </Item>
+        </Advantage>
         <Button variant='contained' size='large' sx={{ width: '200px' }}>
           <a href='#contact'>En savoir plus</a>
         </Button>
@@ -62,7 +86,10 @@ const Left = styled.div`
 `
 
 const Image = styled.img`
-  width: 90%;
+  width: 40%;
+  position: absolute;
+  left: -1vw;
+  top: 6vh;
 `
 
 const Right = styled.div`
@@ -83,21 +110,38 @@ const Right = styled.div`
 `
 
 const Title = styled.span`
-  font-size: 45px;
+  font-size: 30px;
+  margin-bottom: 30px;
 
   @media only screen and (max-width: 480px) {
-    font-size: 30px;
+    font-size: 26px;
   }
   @media only screen and (min-width: 481px) and (max-width: 768px) {
-    font-size: 30px;
+    font-size: 28px;
   }
+`
+
+const Advantage = styled.div`
+  display: flex;
+  margin-bottom: 20px;
+`
+
+const Picto = styled.img`
+  width: 50px;
+  height: 50px;
+  margin-right: 10px;
+`
+
+const Item = styled.div`
+  display: flex;
+  flex-direction: column;
 `
 
 const SubTitle = styled.span`
   font-size: 24px;
-  font-style: italic;
+  font-weight: bold;
   color: #333;
-  margin: 30px 0;
+  margin-bottom: 10px;
 
   @media only screen and (min-width: 481px) and (max-width: 768px) {
     font-size: 22px;
@@ -107,8 +151,8 @@ const SubTitle = styled.span`
 
 const Description = styled.span`
   font-size: 20px;
+  font-style: italic;
   color: #777;
-  margin-bottom: 30px;
 
   @media only screen and (min-width: 481px) and (max-width: 768px) {
     margin-bottom: 15px;
