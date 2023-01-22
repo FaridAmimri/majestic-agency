5390ff4b91326c6f3ff97342666ca6d41e6ad8ca
Farid Amimri <farid.amimri@gmail.com>
2023-01-22 16:03:20 +0100

    Update content & add images

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,51 +1,41 @@
 /** @format */
 
 import styled from 'styled-components'
-import ServicePng from '../assets/service.png'
-import JusticeIcon from '../assets/juriste.png'
-import SupportIcon from '../assets/support.png'
-import BoostIcon from '../assets/boost.png'
+import { moralData, physicalData } from '../data'
+import MoralIcon from '../assets/moral.png'
+import PhysicalIcon from '../assets/physical.png'
 
 function Service() {
   return (
     <Container id='service'>
-      <Left>
-        <Image src={ServicePng} />
-      </Left>
-      <Right>
-        <Header>Simplifiez-vous la vie</Header>
-        <Wrapper>
-          <Item>
-            <Icon src={JusticeIcon}></Icon>
-            <Content>
-              <Title>Un juriste dédié</Title>
-              <Description>
-                Pour toutes vos formalités administratives et juridiques
-              </Description>
-            </Content>
-          </Item>
-          <Item>
-            <Icon src={SupportIcon}></Icon>
-            <Content>
-              <Title>Un conseiller disponible</Title>
-              <Description>
-                Un accompagnement juridique pour tout type d'entreprise y
-                compris les associations
-              </Description>
-            </Content>
-          </Item>
-          <Item>
-            <Icon src={BoostIcon}></Icon>
-            <Content>
-              <Title>Boostez votre croissance</Title>
-              <Description>
-                Un accompagnement entrepreneurial pour accélérer votre
-                croissance
-              </Description>
-            </Content>
-          </Item>
-        </Wrapper>
-      </Right>
+      <Header>Pour Qui ?</Header>
+      <Wrapper>
+        <Card>
+          <TitleContainer>
+            <Title>Personnes morales :</Title>
+            <Icon src={MoralIcon} alt='Personne morale' />
+          </TitleContainer>
+          {moralData.map((item) => (
+            <Item key={item.id}>
+              <SubTitle>{item.subtitle}</SubTitle>
+              <Description>{item.description}</Description>
+            </Item>
+          ))}
+        </Card>
+
+        <Card>
+          <TitleContainer>
+            <Title>Personnes physiques :</Title>
+            <Icon src={PhysicalIcon} alt='Personne physique' />
+          </TitleContainer>
+          {physicalData.map((item) => (
+            <Item key={item.id}>
+              <SubTitle>{item.subtitle}</SubTitle>
+              <Description>{item.description}</Description>
+            </Item>
+          ))}
+        </Card>
+      </Wrapper>
     </Container>
   )
 }
@@ -53,7 +43,10 @@ function Service() {
 export default Service
 
 const Container = styled.div`
+  height: 100%;
   display: flex;
+  flex-direction: column;
+  justify-content: center;
 
   @media only screen and (max-width: 480px) {
     padding: 20px;
@@ -65,28 +58,12 @@ const Container = styled.div`
   }
 `
 
-const Left = styled.div`
-  width: 50%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  @media only screen and (max-width: 480px) {
-    display: none;
-  }
-  @media only screen and (min-width: 481px) and (max-width: 768px) {
-    width: 30%;
-  }
+const Header = styled.h1`
+  text-align: center;
 `
 
-const Image = styled.img`
-  width: 100%;
-`
-
-const Right = styled.div`
-  width: 50%;
+const Wrapper = styled.div`
   display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
 
@@ -98,40 +75,42 @@ const Right = styled.div`
   }
 `
 
-const Wrapper = styled.div`
+const Card = styled.div`
   display: flex;
   flex-direction: column;
-  margin-top: 50px;
+  flex: 1;
+  height: 470px;
+  padding: 25px;
 `
 
-const Header = styled.h1``
-
-const Item = styled.div`
-  margin-bottom: 40px;
+const TitleContainer = styled.div`
   display: flex;
   align-items: center;
+  margin-bottom: 10px;
+`
+
+const Icon = styled.img`
+  height: 70px;
+  width: 100px;
+  margin-left: 80px;
+`
+
+const Title = styled.h2``
+
+const Item = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 10px;
 
   @media only screen and (min-width: 481px) and (max-width: 768px) {
     margin-bottom: 20px;
   }
 `
 
-const Icon = styled.img`
-  width: 50px;
-  height: 50px;
-`
-
-const Content = styled.div`
-  display: flex
-  justify-content: center;
-  align-items: center;
-  padding-left: 20px;
-`
-
-const Title = styled.h2``
+const SubTitle = styled.h3``
 
 const Description = styled.p`
-  font-size: 20px;
-  margin-top: 20px;
+  font-size: 16px;
+  margin-top: 10px;
   color: #555;
 `
