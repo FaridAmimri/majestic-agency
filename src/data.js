004cf3d1da8564967b4150d68163e7f05aee027f
Farid Amimri <farid.amimri@gmail.com>
2023-01-22 16:03:56 +0100

    Create data file for Service component

---
+++ src/data.js
@@ -0,0 +1,47 @@
+/** @format */
+
+export const moralData = [
+  {
+    id: 1,
+    subtitle: 'Grosse Entreprises',
+    description:
+      'Externaliser la réception de votre courrier pour une gestion simplifiée. Gagnez du temps en déléguant une partie de votre gestion administrative et donnez du sens au télétravail en rejoignant un monde dématérialisé.'
+  },
+  {
+    id: 2,
+    subtitle: 'Petite boîtes, auto-entreprises et associations',
+    description:
+      'Protégez votre vie privée en séparant votre adresse personnelle de votre boîte postale professionnelle. Majestic Domiciliation est une adresse pour vous aider à gagner en sécurité et en professionnalisme.'
+  },
+  {
+    id: 3,
+    subtitle: 'SCI',
+    description:
+      'Réunissez vos adresses en un point grâce à une boîte postale professionnelle sur Dunkerque. Laissez les voisins hors de vos affaires et dissocier votre vie privée de vos activités commerciales. Avec Majestic Dom, sécurité et confidentialité sont garanties.'
+  }
+]
+
+export const physicalData = [
+  {
+    id: 1,
+    subtitle: 'Vadrouilleur aguerri',
+    description: `Economisez en ne ratant aucune facture ou courriers bancaires. Libérez vos parents : recevez votre courrier le jour même, n’importe où dans le monde.`
+  },
+  {
+    id: 2,
+    subtitle: 'Situation précaire périodique',
+    description: `Gardez votre situation secrète. Revalorisez votre image en vous offrant une adresse pour s’en sortir et recevoir tous vos documents importants.`
+  },
+  {
+    id: 3,
+    subtitle: 'Les expats',
+    description: `Même à l’autre bout du monde, recevez votre courrier postal en format digital le jour même. Gérer son courrier n’aura jamais été aussi simple !`
+  },
+  {
+    id: 4,
+    subtitle:
+      'Les Ninjas : (célébrités, people, influenceurs, militants engagés, etc...)',
+    description:
+      'Vous aimez votre anonymat et ne souhaitez pas divulguer votre adresse ? Quel que soit vos raisons : vous avez le droit ! Majestic domiciliation vous propose un service 100% privé et sécurisé.'
+  }
+]
